3fe5d3f38dae1593408c911a2f0fa237dd19ea7d
Allan Lima <allancslima@gmail.com>
2022-12-10 02:08:26 -0300

    Adapted heap to enqueue distance structs

--- util/heap/heap.c
+++ util/heap/heap.c
@@ -5,7 +5,12 @@
 struct _heap {
     int capacity;
     int size;
-    int *data;
+    distance_t **data;
+};
+
+struct _distance {
+    int to_vertex;
+    int value;
 };
 
 /* private functions */
@@ -18,13 +23,22 @@ int get_right_index(int i);
 
 void min_heapify(heap_t *queue, int i);
 
-int swap(int *a, int *b);
+void swap(heap_t *heap, int a_index, int b_index);
 
 /* implementations */
 
+distance_t* distance_create(int to_vertex, int value) {
+    distance_t *distance = (distance_t*) malloc(sizeof(distance_t));
+
+    distance->to_vertex = to_vertex;
+    distance->value = value;
+
+    return distance;
+}
+
 heap_t* heap_create(int capacity) {
     heap_t *heap = (heap_t*) malloc(sizeof(heap_t));
-    int *data = (int*) malloc(sizeof(int) * (capacity+1)); // 1-based indexing
+    distance_t **data = (distance_t**) malloc(sizeof(distance_t*) * (capacity + 1)); // 1-based indexing
     
     heap->capacity = capacity;
     heap->size = 0;
@@ -33,7 +47,7 @@ heap_t* heap_create(int capacity) {
     return heap;
 }
 
-void heap_enqueue(heap_t *heap, int item) {
+void heap_enqueue(heap_t *heap, distance_t *item) {
     if (heap->size >= heap->capacity) {
         printf("Heap overflow!");
         exit(-1);
@@ -44,25 +58,29 @@ void heap_enqueue(heap_t *heap, int item) {
     int key_index = heap->size;
     int parent_index = get_parent_index(heap->size);
 
-    while (parent_index >= 1 && heap->data[key_index] < heap->data[parent_index]) {
-        swap(&heap->data[key_index], &heap->data[parent_index]);
+    while (parent_index >= 1 && heap->data[key_index]->value < heap->data[parent_index]->value) {
+        swap(heap, key_index, parent_index);
         key_index = parent_index;
         parent_index = get_parent_index(key_index);
     }
 }
 
-int heap_dequeue(heap_t *heap) {
+distance_t* heap_dequeue(heap_t *heap) {
     if (heap->size <= 0) {
         printf("Heap underflow!");
         exit(-1);
     }
 
-    int item = heap->data[1];
+    distance_t *item = heap->data[1];
     heap->data[1] = heap->data[heap->size];
     heap->size--;
     min_heapify(heap, 1);
 
     return item;
+}
+
+int heap_is_empty(heap_t *heap) {
+    return heap->size == 0;
 }
 
 int get_parent_index(int i) {
@@ -82,24 +100,24 @@ void min_heapify(heap_t *heap, int i) {
     int left_index = get_left_index(i);
     int right_index = get_right_index(i);
     
-    if (left_index <= heap->size && heap->data[left_index] < heap->data[i]) {
+    if (left_index <= heap->size && heap->data[left_index]->value < heap->data[i]->value) {
         smallest = left_index;
     } else {
         smallest = i;
     }
     
-    if (right_index <= heap->size && heap->data[right_index] < heap->data[smallest]) {
+    if (right_index <= heap->size && heap->data[right_index]->value < heap->data[smallest]->value) {
         smallest = right_index;
     }
     
-    if (heap->data[i] != heap->data[smallest]) {
-        swap(&heap->data[i], &heap->data[smallest]);
+    if (heap->data[i]->value != heap->data[smallest]->value) {
+        swap(heap, i, smallest);
         min_heapify(heap, smallest);
     }
 }
 
-int swap(int *a, int *b) {
-    int aux = *a;
-    *a = *b;
-    *b = aux;
+void swap(heap_t *heap, int a_index, int b_index) {
+    distance_t *aux = heap->data[a_index];
+    heap->data[a_index] = heap->data[b_index];
+    heap->data[b_index] = aux;
 }
--- util/heap/heap.h
+++ util/heap/heap.h
@@ -2,11 +2,16 @@
 #define HEAP_H
 
 typedef struct _heap heap_t;
+typedef struct _distance distance_t;
+
+distance_t* distance_create(int to_vertex, int value);
 
 heap_t* heap_create(int capacity);
 
-void heap_enqueue(heap_t *heap, int item);
+void heap_enqueue(heap_t *heap, distance_t *item);
 
-int heap_dequeue(heap_t *heap);
+distance_t* heap_dequeue(heap_t *heap);
+
+int heap_is_empty(heap_t *heap);
 
 #endif
